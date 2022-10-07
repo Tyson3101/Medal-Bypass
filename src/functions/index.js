885be3bc7150bac9815c80924cf53c5e4ac2e330
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2022-10-08 02:36:51 +1100

    Added log in so can download clips with permission needed, and cooldowns

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const os = require("os");
@@ -6,29 +7,42 @@ const fs = require("fs/promises");
 const m3u8ToMp4 = require("m3u8-to-mp4");
 const converter = new m3u8ToMp4();
 const puppeteer = require("puppeteer-extra");
-let stealth = require("puppeteer-extra-plugin-stealth")();
+const stealth = require("puppeteer-extra-plugin-stealth")();
 puppeteer.use(stealth);
 
 const serviceAccount = require("./serviceAccountKey.json");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 const bucket = admin.app().storage().bucket("medalbypass.appspot.com");
 
-function getFileURL(url) {
+async function startFileGet(url) {
+  try {
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+    const page = await browser.newPage();
+    const client = await page.target().createCDPSession();
+    return await getFileURL(url, page, browser, client);
+  } catch (e) {
+    return null;
+  }
+}
+
+function getFileURL(url, page, browser, client) {
+  const logInAtagSelector =
+    "#__next > div > div.deviceSize__Desktop-sc-1wgp2re-0.clmXhW > nav > div.StyledBox-sc-13pk1d4-0.ZIwwl > div.StyledBox-sc-13pk1d4-0.lfUOxE > a:nth-child(5)";
+  let privateFound = false;
   let fileFound = false;
   let cdnURLFound = false;
-  return new Promise(async (resolve, r) => {
+  let cdnURL = "";
+  let interval;
+  return new Promise(async (resolve, reject) => {
     try {
-      const browser = await puppeteer.launch({
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: chromium.headless,
-      });
-      const page = await browser.newPage();
-      const client = await page.target().createCDPSession();
-
       await client.send("Network.enable");
       await client.send("Network.setRequestInterception", {
         patterns: [
@@ -82,43 +96,83 @@ function getFileURL(url) {
 
                 resolve(publicSrcUrl);
               })
-              .catch(r);
+              .catch(reject);
           }
 
-          if (interceptionId)
-            client.send("Network.continueInterceptedRequest", {
-              interceptionId,
-            });
+          if (interceptionId) {
+            try {
+              await client.send("Network.continueInterceptedRequest", {
+                interceptionId,
+              });
+            } catch {}
+          }
         }
       );
       client.on("error", () => {});
       setTimeout(async () => {
-        if (!fileFound && !cdnURLFound) {
+        if (!fileFound && !cdnURLFound && !privateFound) {
           await browser.close();
-          r();
+          reject();
         }
-      }, 16000);
+      }, 15000);
       try {
-        await page.goto(url);
+        await page.goto(url, { waitUntil: "load" });
         await page.waitForSelector("video");
+        await page.waitForSelector(logInAtagSelector);
       } catch {
-        r();
+        reject();
       }
-      const src = await page.evaluate(
+      const initalSrc = await page.evaluate(
         () =>
           document.querySelector("video")?.src ||
           document.querySelector("source")?.src
       );
-      if (src?.includes("cdn.medal.tv/")) cdnURLFound = true;
-      setTimeout(async () => {
-        if (!fileFound && cdnURLFound) {
-          await browser.close();
-          console.log({ src });
-          resolve(src);
+      if (checkIfMedalClipCDN(initalSrc)) {
+        cdnURLFound = true;
+        cdnURL = initalSrc;
+        clearInterval(interval);
+      }
+      if (
+        initalSrc.toLowerCase().split("?info")[0] ===
+        "https://cdn.medal.tv/assets/video/privacy-protected-guest-720p.c4821e1e.mp4"
+      ) {
+        privateFound = true;
+        await page.click(logInAtagSelector);
+        const userNameSelector = "#username > div > input";
+        const passwordSelector = "input[data-testid='password-field']";
+        const frameHandler = await page.waitForSelector("iframe[src='/login']");
+        const frame = await frameHandler.contentFrame();
+        await frame.waitForSelector(userNameSelector);
+        await frame.type(userNameSelector, process.env.USERNAME);
+        await frame.waitForSelector(passwordSelector);
+        await frame.type(passwordSelector, process.env.PASSWORD);
+        await frame.click("button[data-testid='log-in-button']");
+        try {
+          await page.waitForNavigation({ timeout: 10000 });
+          interval = setInterval(async () => {
+            const checkSrc = await page.evaluate(
+              () =>
+                document.querySelector("video")?.src ||
+                document.querySelector("source")?.src
+            );
+            if (checkIfMedalClipCDN(checkSrc)) {
+              await browser.close();
+              resolve(checkSrc);
+              clearInterval(interval);
+            }
+          }, 1000);
+        } catch {
+          reject();
         }
-      }, 7000);
-    } catch {
-      r();
+      }
+      setTimeout(async () => {
+        if (!fileFound && cdnURLFound && !privateFound) {
+          await browser.close();
+          resolve(cdnURL);
+        }
+      }, 2500);
+    } catch (e) {
+      reject();
     }
   });
 }
@@ -141,7 +195,7 @@ exports.video = functions
       return res.json({ valid: false });
     if (!url.toLowerCase().includes("?theater=true")) url += "?theater=true";
     try {
-      const src = await getFileURL(req.body.url);
+      const src = await startFileGet(req.body.url);
       if (src) {
         return res.json({ valid: true, src });
       } else throw new Error("No data");
@@ -150,3 +204,19 @@ exports.video = functions
       return res.json({ valid: false });
     }
   });
+
+function checkIfMedalClipCDN(str) {
+  if (!str) return false;
+  if (!str?.toLowerCase().includes("cdn.medal.tv/")) return false;
+  if (
+    str.toLowerCase().split("?info")[0] ===
+    "https://cdn.medal.tv/assets/video/privacy-protected-guest-720p.c4821e1e.mp4"
+  )
+    return false;
+  if (str?.toLowerCase().includes("cdn.medal.tv/source/")) return true;
+  else if (
+    str?.toLowerCase().includes("720p") ||
+    str?.toLowerCase().includes("1080p")
+  )
+    return true;
+}
